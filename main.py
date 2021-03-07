2f1a93fd8ac68053cd1970b1031307751dc70cd4
IvanG <galochkinia@ugrafmsh.ru>
2021-03-07 15:31:49 +0500

    third task

--- main.py
+++ main.py
@@ -6,15 +6,33 @@ app = Flask(__name__)
 app.config['abc'] = 'abc'
 
 
-def main():
-    global_init('database.db')
-    app.run(port=8080, host='127.0.0.1', debug=True)
+def add_user():
     session = create_session()
+    dictionary = [['Scott', 21, 'captain', 'researcher engineer', 'module_1', 'scott_chief@mars.org'],
+                  ['Elon Mask', 120, 'captain', 'genius', 'module_2', 'elonmask@mail.ru'],
+                  ['Berlin hospital patient', 30, 'prisoner', 'rebel', 'module_3', 'navalny@gosdep.us'],
+                  ['Yaroslav Filippov', 17, 'student', 'pygamer', 'module_1', 'yaroslav_asu@yandex.ru']]
     try:
-        captain = User()
-        captain.name = 'Scott'
+        for data in dictionary:
+            user = User()
+            user.name = data[0]
+            user.age = data[1]
+            user.position = data[2]
+            user.speciality = data[3]
+            user.address = data[4]
+            user.email = data[5]
+            session.add(user)
+        session.commit()
     finally:
         session.close()
+
+
+def main():
+    global_init('newdb.sqlite')
+    try:
+        add_user()
+    finally:
+        app.run(port=8080, host='127.0.0.1', debug=True)
 
 
 if __name__ == "__main__":
